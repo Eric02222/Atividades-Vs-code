260d9993f1cca27607dd20034e4cb6525c4fed8f
Eric02222 <114702151+Eric02222@users.noreply.github.com>
2024-09-18 22:16:15 -0300

    Add files via upload

--- Al2/script.js
+++ Al2/script.js
@@ -26,3 +26,54 @@
         }
         
     }
+
+    function calcSalario(){
+        let salario
+        let idFuncionario = Number(prompt("Digite seu Id"))
+        let horasTrab = Number(prompt("Digite a quantidade de horas trabalhadas"))
+        let valorPorHora = Number(prompt("Digite o valor ganho por hora"))
+
+        salario = horasTrab * valorPorHora;
+
+        if(salario > 0){
+            alert("Id: "+ idFuncionario +"\n Salario: R$"+ salario.toFixed(2))
+        }else{
+            alert("Opções invalidas!")
+        }
+
+    }
+
+    function calcSalarioComBon(){
+        let comissao = 0.15
+        let nomeFun = prompt("Digite seu seu nome");
+        let salario = Number(prompt("Digite o seu salario"));
+        let numVendas = Number(prompt("Digite o numero de vendas"));
+
+        let pagamento = (salario + (numVendas * comissao));
+
+        if(pagamento > 0){
+            alert("Funcionario: "+ nomeFun +"\nSalario: R$"+ pagamento.toFixed(2));
+        }else{
+            alert("Opções invalidas!");
+        }
+
+    }
+
+    function pedirLanche(){
+        let cachorroQ = 4, xSalada = 4.50, xBacon = 5, torradaSimp = 2, refrigerante = 1.50
+        let escolhaLanc = prompt("Digite qual lanhe você quer \n1  Cachorro Quente  R$4,00 \n 2  Xsalada  R$4,50 \n"+
+        "3 Xbacon R$5,00 \n3 Torrada Simples R$2,00 \n5 Refrigerante R$1,50");
+        let quantidadeLanc = prompt("Digite quantos lanches deseja");
+
+        if(escolhaLanc == 1){
+            let lanche = cachorroQ * quantidadeLanc;
+        }else if(escolhaLanc == 2){
+            let lanche = cachorroQ * quantidadeLanc;
+        }else if(escolhaLanc == 2){
+            let lanche = cachorroQ * quantidadeLanc;
+        }else if(escolhaLanc == 2){
+            let lanche = cachorroQ * quantidadeLanc;
+        }else if(escolhaLanc == 2){
+            let lanche = cachorroQ * quantidadeLanc;
+        }
+    }
