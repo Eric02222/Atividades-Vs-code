48ec7a16bfa3a6b80e0410e3ac818edf7972de2c
ERIC MARA DE OLIVEIRA <eric_m_oliveira@estudante.sesisenai.org.br>
2024-10-17 21:04:08 -0300

    salvar atividade

--- Atv2/script.js
+++ Atv2/script.js
@@ -1,19 +1,49 @@
 let numeros = [];
+let n1;
+let n2;
 
 function atv83() {
     let num;
-    let n1 = document.getElementById("num1").value;
-    let n2 = document.getElementById("num1").value;
 
-    while (numeros.length < 25) {
+    for (let i = 0; i < 25; i++) {
         num = Math.ceil(Math.random() * 25);
-        let cal;
         numeros.push(num)
     }
 
+    /*while (numeros.length < 25) {
+        num = Math.ceil(Math.random() * 25);
+        numeros.push(num)
+    }*/
 
-    
+    document.getElementById("resultado").innerHTML = "Array gerada com sucesso <br>"
+
     console.log(numeros)
+}
+
+function atv83b() {
+    do {
+        n1 = Number(prompt("Digite um numero maior que 0:"));
+
+    } while (n1 < 0 || n1 > 24 || !n1)
+
+    do {
+        n2 = Number(prompt("Digite outro numero maior que 0:"));
+
+    } while (n2 < 0 || n2 > 24 || !n2)
+
+    soma = numeros[n1] + numeros[n2]
+
+    console.log(soma)
+    document.getElementById("resultado").innerHTML += "Soma de " + numeros[n1] + " + " + numeros[n2] + " = " + soma
+}
+
+function mostrarArray() {
+
+    document.getElementById("resultado").innerHTML = '<h2>Lançamentos dos dados:</h2>'
+    for (let i = 0; i < numeros.length; i++) {
+        document.getElementById("resultado").innerHTML += "Dados: " + numeros[i] + "<br>"
+    }
+
 }
 
 
