61322d0c1d579068597c56cb1e215dd77d8ea93d
Eric Mara <114702151+Eric02222@users.noreply.github.com>
2024-11-06 16:07:33 -0300

    Add files via upload

--- Atv3/script.js
+++ Atv3/script.js
@@ -8,10 +8,22 @@ function modPve() {
     window.location = "pve.html"
 }
 
+function modsobrevivencia() {
+    window.location = "sobrevivencia.html";
+
+}
+
+function pgInicial() {
+    window.location = "modalidade.html";
+}
+
 function pvp() {
     let numSecreto = Number(prompt("Digite o numero o numero secreto"));
     let advNum = Number(prompt("Tente adivinhar o numero secreto"));
     let msg = "Voce Perdeu!"
+    let dica;
+    let operação = Math.ceil(Math.random() * 2);
+    let calc = Math.ceil(Math.random() * 5);
 
     for (let tent = 1; tent <= 3; tent++) {
         if (advNum < numSecreto) {
@@ -22,6 +34,17 @@ function pvp() {
             msg = "Parabens vocé acertou!";
             break
         }
+
+        if (i == 1) {
+            if (operação == 1) {
+                dica = numSecreto[0] - calc
+                alert("O numero esta proximo de " + dica)
+            } else {
+                dica = numSecreto[0] + calc
+                alert("O numero esta proximo de " + dica)
+            }
+        }
+
     }
     alert(msg);
 }
@@ -44,17 +67,33 @@ function dificil() {
 }
 
 function pve() {
-    let num = Number(prompt("Digite um numero"))
-    let msg = "Voce perdeu!!!"
+    let num = Number(prompt("Digite um numero"));
+    let msg = "Voce perdeu!!!";
+    let dica;
+    let operação = Math.ceil(Math.random() * 2);
+    let calc = Math.ceil(Math.random() * 5);
 
     for (let i = 0; i < 3; i++) {
-       if(num < numSecreto[0]){
-        num = Number(prompt("Digite um numero maior"));
-       }else if(num > numSecreto[0]){
-        num = Number(prompt("Digite um numero menor"));
-       }else if(num == numSecreto[0]){
-        msg = "Parabens Voce Acertou!!!"
-       }
+        if (num < numSecreto) {
+            num = Number(prompt("Digite um numero maior"));
+
+        } else if (num > numSecreto) {
+            num = Number(prompt("Digite um numero menor"));
+
+        } else if (num == numSecreto) {
+            msg = "Parabens Voce Acertou!!!"
+        }
+
+        if (i == 1) {
+            if (operação == 1) {
+                dica = numSecreto[0] - calc
+                alert("O numero esta proximo de " + dica)
+            } else {
+                dica = numSecreto[0] + calc
+                alert("O numero esta proximo de " + dica)
+            }
+        }
+
     }
 
     alert(msg)
